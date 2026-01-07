4f95c49d2c6ea375c109dee0a444ca429153c586
Miruna Serian <mirunaserian@gmail.com>
2026-01-07 14:31:50 +0000

    add index page

--- main/views.py
+++ main/views.py
@@ -1,1 +1,9 @@
 """Views for the main app."""
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """Example view function making use of a template."""
+    return render(request, "main/index.html")
