67f20d1e7cbe9f019e03e4d31ae6a452046587be
Steph Wills <stephanie.wills13@imperial.ac.uk>
2026-02-18 08:37:24 +0000

    Add test for task

---
+++ tests/main/test_tasks.py
@@ -0,0 +1,40 @@
+"""Test suite for tasks."""
+
+from datetime import datetime, timedelta
+from unittest.mock import patch
+
+from django.core.cache import cache
+
+
+@patch("main.tasks.static_solar_orbiter_data")
+@patch("main.tasks.trajectory_solar_orbiter_data")
+@patch("main.tasks.datetime")
+def test_set_trajectory_cache(datetime_mock, traj_data_mock, static_data_mock):
+    """Test the set_trajectory_cache function."""
+    from main.tasks import set_trajectory_cache
+
+    time = datetime.now()
+    times = (time, time + timedelta(days=8))
+    datetime_mock.now.return_value = time
+    traj_data_mock.return_value = "trajectory data"
+    static_data_mock.return_value = "static data"
+
+    expected_data = {
+        "static": {"AU": "static data", "angle": "static data"},
+        "trajectory": {"AU": "trajectory data", "angle": "trajectory data"},
+    }
+
+    cache.clear()
+    set_trajectory_cache()
+
+    for unit in ["AU", "angle"]:
+        static_data_mock.assert_any_call(time, unit)
+        traj_data_mock.assert_any_call(times, unit)
+
+    cached_data = cache.get("trajectory_data")
+    assert cached_data == expected_data
+
+    time_gen = cache.get("time_generated")
+    assert time_gen == time.strftime("%Y-%m-%d %H:%M:%S")
+
+    cache.clear()
