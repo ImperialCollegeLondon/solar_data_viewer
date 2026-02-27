7beaf69883b966720ff20e521f85929bc3199f5a
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-02-27 09:39:54 +0000

    Add get_so_magnetic_field

--- main/utils.py
+++ main/utils.py
@@ -6,7 +6,9 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+from django.utils import timezone
 
+from . import models
 from .config import PlotsConfig
 
 
@@ -74,3 +76,47 @@ def process_data_from_test_csvs(
     measurements = df[measurement].tolist()
     data = {"measurement": measurements, "date": dates}
     return data
+
+
+def get_so_magnetic_field(
+    spacecraft: str, measurement: str, range_param: str
+) -> dict[str, list[float]]:
+    """Retrieves the chosen component of the magnetic field data for the SO mission.
+
+    Args:
+        spacecraft: Name of the spacecraft to retrieve data for.
+        measurement: Name of the measurement to get data for.
+        range_param: The time range for which to retrieve data.
+
+    Returns:
+        A dictionary containing the relevant datetimes in UNIX epoch time format and
+            the measurements to plot.
+    """
+    # Get the time range to display
+    ranges = {
+        "1d": pd.Timedelta(days=1),
+        "3d": pd.Timedelta(days=3),
+        "7d": pd.Timedelta(days=7),
+    }
+    delta = ranges.get(range_param, ranges["3d"])
+    from_date = timezone.now() - delta
+
+    # Get the relevant data from the DB
+    data = pd.DataFrame(
+        models.SOMagneticField.objects.filter(time__gte=from_date)
+        .order_by("time")
+        .values("time", measurement)
+    )
+
+    # Do some post processing to sanitize the data
+    data = data.replace({np.nan: "nan"})
+    data = data.rename(columns={data.columns[0]: "date"})
+    data["date"] = pd.to_datetime(data["date"], utc=True)
+
+    # Format datetime as Unix epoch time
+    data["date"] = data["date"].astype("int64") // 10**6
+
+    # Create JSON response
+    dates = data["date"].tolist()
+    measurements = data[measurement].tolist()
+    return {"measurement": measurements, "date": dates}
