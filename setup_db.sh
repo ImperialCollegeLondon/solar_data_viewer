c4ed69a35c9e885c05dfefd0a2950bf2cdc9cf04
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-03-04 14:51:20 +0000

    Add database setup

---
+++ setup_db.sh
@@ -0,0 +1,7 @@
+# Install the Solar Orbiter pipelines
+uv tool install --python 3.10 "git+https://github.com/ImperialCollegeLondon/so-pipeline-core-rse@dalonsoa-patch-1"
+uv tool update-shell
+
+# Use them to create a database with the right schema
+export SOLO_SQLALCHEMY_URL="sqlite:///./db/db.sqlite3"
+so-db create-db --with-schema
