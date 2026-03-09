6772e5b21cb0586dc1c0f9b405a9820720bb1f31
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-03-09 14:06:12 +0000

    Hook real GSE mag data into pipeline

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class BaseRTNMagneticField(models.Model):
-    """Model describing the components of the magnetic field on a specific datetime."""
+    """Model describing the RTN components of the magnetic field."""
 
     time = models.DateTimeField(
         primary_key=True, null=False, help_text="Time for the data."
@@ -23,4 +23,27 @@ class SORTNMagneticField(BaseRTNMagneticField):
 
     class Meta:  # noqa: D106
         db_table = "solo_L2_mag-rtn-ll-internal"
+        managed = False
+
+
+class BaseGSEMagneticField(models.Model):
+    """Model describing the GSE components of the magnetic field."""
+
+    time = models.DateTimeField(
+        primary_key=True, null=False, help_text="Time for the data."
+    )
+    B_x = models.FloatField(help_text="'x' component of the magnetic field.")
+    B_y = models.FloatField(help_text="'y' component of the magnetic field.")
+    B_z = models.FloatField(help_text="'z' component of the magnetic field.")
+    B_mod = models.FloatField(help_text="Modulus of the magnetic field.")
+
+    class Meta:  # noqa: D106
+        abstract = True
+
+
+class SOGSEMagneticField(BaseRTNMagneticField):
+    """Magnetic field model for the Solar Orbiter mission."""
+
+    class Meta:  # noqa: D106
+        db_table = "solo_L2_mag-gse-ll-internal"
         managed = False
--- main/utils.py
+++ main/utils.py
@@ -45,6 +45,10 @@ def process_data_from_test_csvs(
         A dictionary containing the relevant datetimes in UNIX epoch time format and
             the measurements to plot.
     """
+    # Hack to use the data in the database, while we pipe things together
+    if spacecraft == "SO" and measurement.startswith("B_"):
+        return get_so_gse_magnetic_field(measurement, range_param)
+
     csv_files = {
         "IMAP": Path(__file__).parent / "data" / "test_data1.csv",
         "SO": Path(__file__).parent / "data" / "test_data2.csv",
@@ -107,6 +111,53 @@ def get_so_rtn_magnetic_field(
     # Get the relevant data from the DB
     data = pd.DataFrame(
         models.SORTNMagneticField.objects.filter(time__gte=from_date)
+        .order_by("time")
+        .values("time", measurement)
+    )
+
+    # Do some post processing to sanitize the data
+    data = data.replace({np.nan: "nan"})
+    data = data.rename(columns={data.columns[0]: "date"})
+    data["date"] = pd.to_datetime(data["date"], utc=True)
+
+    # Format datetime as Unix epoch time
+    data["date"] = data["date"].astype("int64") // 10**6
+
+    # Create JSON response
+    dates = data["date"].tolist()
+    measurements = data[measurement].tolist()
+    return {"measurement": measurements, "date": dates}
+
+
+def get_so_gse_magnetic_field(
+    measurement: str, range_param: str
+) -> dict[str, list[float]]:
+    """Retrieves the chosen component of the magnetic field data for the SO mission.
+
+    Args:
+        measurement: Name of the measurement to get data for.
+        range_param: The time range for which to retrieve data.
+
+    Returns:
+        A dictionary containing the relevant datetimes in UNIX epoch time format and
+            the measurements to plot.
+    """
+    if measurement not in ("B_x", "B_y", "B_z"):
+        raise ValueError(
+            "Only RTN magnetic field components can be retrieved by this function."
+        )
+    # Get the time range to display
+    ranges = {
+        "1d": pd.Timedelta(days=1),
+        "3d": pd.Timedelta(days=3),
+        "7d": pd.Timedelta(days=7),
+    }
+    delta = ranges.get(range_param, ranges["3d"])
+    from_date = timezone.now() - delta
+
+    # Get the relevant data from the DB
+    data = pd.DataFrame(
+        models.SOGSEMagneticField.objects.filter(time__gte=from_date)
         .order_by("time")
         .values("time", measurement)
     )
