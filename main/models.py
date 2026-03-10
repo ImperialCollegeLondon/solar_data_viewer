f1d26f8d9ccec5f6577e5983cc2d4a734c4865e2
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-03-10 15:22:22 +0000

    Add IMAPGSEMagneticField

--- main/models.py
+++ main/models.py
@@ -1,1 +1,29 @@
 """Models for the main app."""
+
+from django.db import models
+
+
+class IMAPGSEMagneticField(models.Model):
+    """Model describing the GSE components of the magnetic field."""
+
+    time = models.DateTimeField(
+        primary_key=True,
+        null=False,
+        help_text="Time for the data.",
+        db_column="kernel_set_key",
+    )
+
+    bx_gse = models.FloatField(
+        help_text="GSE 'x' component of the magnetic field.", db_column="B_GSE_x"
+    )
+    by_gse = models.FloatField(
+        help_text="GSE 'y' component of the magnetic field.", db_column="B_GSE_y"
+    )
+    bz_gse = models.FloatField(
+        help_text="GSE 'z' component of the magnetic field.", db_column="B_GSE_z"
+    )
+    b_mag = models.FloatField(help_text="Modulus of the magnetic field.")
+
+    class Meta:  # noqa: D106
+        db_table = "ialirt_mag"
+        managed = False
--- main/utils.py
+++ main/utils.py
@@ -6,7 +6,9 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+from django.utils import timezone
 
+from . import models
 from .config import PlotsConfig
 
 
@@ -43,6 +45,9 @@ def process_data_from_test_csvs(
         A dictionary containing the relevant datetimes in UNIX epoch time format and
             the measurements to plot.
     """
+    if measurement in ("bx_gse", "by_gse", "bz_gse") and spacecraft in ("IMAP"):
+        return get_gse_magnetic_field(spacecraft, measurement, range_param)
+
     csv_files = {
         "IMAP": Path(__file__).parent / "data" / "test_data1.csv",
         "SO": Path(__file__).parent / "data" / "test_data2.csv",
@@ -74,3 +79,56 @@ def process_data_from_test_csvs(
     measurements = df[measurement].tolist()
     data = {"measurement": measurements, "date": dates}
     return data
+
+
+def get_gse_magnetic_field(
+    spacecraft: str, measurement: str, range_param: str
+) -> dict[str, list[float]]:
+    """Retrieves the chosen component of the magnetic field data for the SO mission.
+
+    Args:
+        spacecraft: Name of the spacecraft to retrieve data for.
+        measurement: Name of the measurement to get data for.
+        range_param: The time range for which to retrieve data.
+
+    Returns:
+        A dictionary containing the relevant datetimes in UNIX epoch time format and
+            the measurements to plot.
+    """
+    if measurement not in ("bx_gse", "by_gse", "bz_gse"):
+        raise ValueError(
+            "Only GSE magnetic field components can be retrieved by this function."
+        )
+
+    # Tables to get the data from, mission specific
+    tables = {"IMAP": models.IMAPGSEMagneticField}
+
+    # Get the time range to display
+    ranges = {
+        "1d": pd.Timedelta(days=1),
+        "3d": pd.Timedelta(days=3),
+        "7d": pd.Timedelta(days=7),
+    }
+    delta = ranges.get(range_param, ranges["3d"])
+    from_date = timezone.now() - delta
+
+    # Get the relevant data from the DB
+    data = pd.DataFrame(
+        tables[spacecraft]
+        .objects.filter(time__gte=from_date)
+        .order_by("time")
+        .values("time", measurement)
+    )
+
+    # Do some post processing to sanitize the data
+    data = data.replace({np.nan: "nan"})
+    data = data.rename(columns={data.columns[0]: "date"})
+    data["date"] = pd.to_datetime(data["date"], utc=True)
+
+    # Format datetime as Unix epoch time
+    data["date"] = data["date"].astype("int64") // 10**6
+
+    # Create JSON response
+    dates = data["date"].tolist()
+    measurements = data[measurement].tolist()
+    return {"measurement": measurements, "date": dates}
