70d2c4bc675ff445eb0fa08b82670472ba7e761c
Miruna Serian <mirunaserian@gmail.com>
2026-04-28 12:55:20 +0100

    remove sqlite files and update table name

--- main/models.py
+++ main/models.py
@@ -72,7 +72,7 @@ class SOContactSchedule(models.Model):
     end_time = models.DateTimeField(help_text="End time of the pass")
 
     class Meta:  # noqa: D106
-        db_table = "public.contact_schedule"
+        db_table = "contact_schedule"
         managed = False
 
 
