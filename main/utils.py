508b10233a0057ca7a6092f229f671c829816ca1
Miruna Serian <mirunaserian@gmail.com>
2026-09-09 14:49:32 +0100

    Fix SO speed and density ranges

--- main/utils.py
+++ main/utils.py
@@ -220,15 +220,23 @@ def _get_trace_data(
             average_expression = Abs("speed")
 
         # add filters to avoid extreme values and only get data after most recent date
-        data_filters = {
-            "time__gt": most_recent,
-            f"{measurement}__gt": -1e30,
-            f"{measurement}__lt": 1e30,
-        }
+        queryset = model.objects.filter(time__gt=most_recent)  # type: ignore[attr-defined]
+
+        if spacecraft == "SO":
+            if measurement == "speed":
+                queryset = queryset.annotate(valid_speed=Abs("speed")).filter(
+                    valid_speed__gte=100,
+                    valid_speed__lte=2000,
+                )
+
+            if measurement == "density":
+                queryset = queryset.filter(
+                    density__gte=0.1,
+                    density__lte=500,
+                )
 
         dataquery = (
-            model.objects.filter(**data_filters)  # type: ignore[attr-defined]
-            .annotate(date=TruncMinute("time"))
+            queryset.annotate(date=TruncMinute("time"))
             .values("date")
             .annotate(average=Avg(average_expression))
             .order_by("date")
